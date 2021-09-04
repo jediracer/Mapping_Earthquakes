13f6baf3ee5dd9932dab35082ab3e2901ef514ec
Jared Behler <jediracer13@gmail.com>
2021-09-04 19:59:33 -0400

    updated logicstep5.js

--- Earthquakes_past7days/static/js/logicStep5.js
+++ Earthquakes_past7days/static/js/logicStep5.js
@@ -1,6 +1,3 @@
-// Add console.log to check to see if our code is working.
-console.log("working");
-
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -45,8 +42,6 @@ let earthquakeData  = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary
 
 // Grabbing our GeoJSON data.
 d3.json(earthquakeData).then(function(data) {
-    //console.log(data);
-
     // This function determines the radius of the earthquake marker based on its magnitude.
     // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
     function getRadius(magnitude) {
@@ -58,24 +53,24 @@ d3.json(earthquakeData).then(function(data) {
 
     // This function determines the color of the circle based on the magnitude of the earthquake.
     function getColor(magnitude) {
-        if (magnitude > 5) {
+        if (magnitude >= 5) {
         return "#ea2c2c";
         }
-        if (magnitude > 4) {
+        if (magnitude >= 4) {
         return "#ea822c";
         }
-        if (magnitude > 3) {
+        if (magnitude >= 3) {
         return "#ee9c00";
         }
-        if (magnitude > 2) {
+        if (magnitude >= 2) {
         return "#eecc00";
         }
-        if (magnitude > 1) {
+        if (magnitude >= 1) {
         return "#d4ee00";
         }
-        // if (magnitude < 1) {
-        // return "#98ee00";
-        // }
+        if (magnitude < 1) {
+        return "#98ee00";
+        }
         return "#98ee00";
     }
 
@@ -98,7 +93,6 @@ d3.json(earthquakeData).then(function(data) {
     L.geoJson(data, {
         // We turn each feature into a circleMarker on the map.
         pointToLayer: function(feature, latlng) {
-            //console.log(data);
             return L.circleMarker(latlng);
         },
         // We set the style for each circleMarker using our styleInfo function.
@@ -120,14 +114,10 @@ d3.json(earthquakeData).then(function(data) {
     legend.onAdd = function() {
         let div = L.DomUtil.create("div", "legend");
         const magnitudes = [0, 1, 2, 3, 4, 5];
-        const colors = ["#98ee00", "#d4ee00", "#eecc00", "#ee9c00", "#ea822c", "#ea2c2c"];
     // Looping through our intervals to generate a label with a colored square for each interval.
         for (var i = 0; i < magnitudes.length; i++) {
-            console.log(magnitudes[i]);
-            console.log(colors[i]);
             div.innerHTML +=
-            "<i style='background: " + colors[i] + "'></i> " +
-        //    "<i style='background: " + getColor(i) + "'></i> " +
+            "<i style='background: " + getColor(i) + "'></i> " +
             magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
         }
         return div;
